6cffadae2c9a366a54a6ef3c0029521552ef544e
Pau Tarrés <91079521+tarrut@users.noreply.github.com>
2025-01-10 18:58:42 +0100

    small changes
    - change in the displayed order of projects
    - change of the "airline optimization" image

--- pautarres/list_of_projects.py
+++ pautarres/list_of_projects.py
@@ -1,17 +1,17 @@
 
 projects = [
     {
+        "title": "Airline Optimization",
+        "description": "Optimization of an airline's fleet distribution and aicraft routing using R.",
+        "image_src": "./images/img_airline_optimization.jpg",
+        "link": "./airline_optimization",
+        "source": "airline_optimization.md",
+    },
+    {
         "title": "Personal Website",
         "description": "Creation of this very same webpage in Python using Reflex.",
         "image_src": "./images/img_personal_website.svg",
         "link": "./personal_website",
         "source": "website_article.md",
-    },
-    {
-        "title": "Airline Optimization",
-        "description": "Optimization of an airline's fleet distribution and aicraft routing using R.",
-        "image_src": "./images/img_airline_optimization.svg",
-        "link": "./airline_optimization",
-        "source": "airline_optimization.md",
     },
 ]
